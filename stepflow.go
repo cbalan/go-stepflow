41e0f957f6cd2d09ff13a6c37f97082e0bf258ae
Catalin Balan <catalin.balan@gmail.com>
2025-07-17 00:32:16 +0200

    Renamed StepFlowItems helper to Items

--- stepflow.go
+++ stepflow.go
@@ -9,13 +9,13 @@ type StepsSpec struct {
 	items []core.StepFlowItem
 }
 
-// StepFlowItems returns the core.StepFlowItem items defined via StepsSpec.
-func StepFlowItems(stepsSpec *StepsSpec) []core.StepFlowItem {
+// Items returns the core.StepFlowItem items defined via StepsSpec.
+func Items(stepsSpec *StepsSpec) []core.StepFlowItem {
 	return stepsSpec.items
 }
 
 func (s *StepsSpec) Steps(name string, stepsSpec *StepsSpec) *StepsSpec {
-	s.items = append(s.items, core.NewStepsItem(name, StepFlowItems(stepsSpec)))
+	s.items = append(s.items, core.NewStepsItem(name, Items(stepsSpec)))
 	return s
 }
 
@@ -30,17 +30,17 @@ func (s *StepsSpec) WaitFor(name string, conditionFunc func(ctx context.Context)
 }
 
 func (s *StepsSpec) Retry(name string, errHandlerFunc func(ctx context.Context, err error) (bool, error), stepsSpec *StepsSpec) *StepsSpec {
-	s.items = append(s.items, core.NewRetryItem(core.NewStepsItem(name+"Retry", StepFlowItems(stepsSpec)), errHandlerFunc))
+	s.items = append(s.items, core.NewRetryItem(core.NewStepsItem(name+"Retry", Items(stepsSpec)), errHandlerFunc))
 	return s
 }
 
 func (s *StepsSpec) LoopUntil(name string, conditionFunc func(ctx context.Context) (bool, error), stepsSpec *StepsSpec) *StepsSpec {
-	s.items = append(s.items, core.NewLoopUntilItem(name+"LoopUntil", core.NewStepsItem("steps", StepFlowItems(stepsSpec)), conditionFunc))
+	s.items = append(s.items, core.NewLoopUntilItem(name+"LoopUntil", core.NewStepsItem("steps", Items(stepsSpec)), conditionFunc))
 	return s
 }
 
 func (s *StepsSpec) Case(name string, conditionFunc func(ctx context.Context) (bool, error), stepsSpec *StepsSpec) *StepsSpec {
-	s.items = append(s.items, core.NewCaseItem(name+"Case", core.NewStepsItem("steps", StepFlowItems(stepsSpec)), conditionFunc))
+	s.items = append(s.items, core.NewCaseItem(name+"Case", core.NewStepsItem("steps", Items(stepsSpec)), conditionFunc))
 	return s
 }
 
@@ -54,5 +54,5 @@ type StepFlow interface {
 }
 
 func NewStepFlow(name string, stepsSpec *StepsSpec) (StepFlow, error) {
-	return core.NewStepFlow(core.NewStepsItem(name, StepFlowItems(stepsSpec)))
+	return core.NewStepFlow(core.NewStepsItem(name, Items(stepsSpec)))
 }
